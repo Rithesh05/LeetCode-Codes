fb9e4d0bf30c4f8f85cee4917f2a9657448cdace
Rithesh05 <161914815+Rithesh05@users.noreply.github.com>
2025-01-05 21:00:53 +0530

    Time: 4 ms (97.72%), Space: 85.7 MB (81.48%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,20 +1,30 @@
 class Solution {
-public int[] vowelStrings(String[] words, int[][] queries) {
-int n = words.length;
-int[] Prefix = new int[n + 1];
-Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
-
-for (int i = 0; i < n; i++) {
-Prefix[i + 1] = Prefix[i];
-if (vowels.contains(words[i].charAt(0)) && vowels.contains(words[i].charAt(words[i].length() - 1))) {
-Prefix[i + 1]++;
-}
-}
-
-int[] ANS = new int[queries.length];
-for (int i = 0; i < queries.length; i++) {
-ANS[i] = Prefix[queries[i][1] + 1] - Prefix[queries[i][0]];
-}
-return ANS;
-}
+    public int isVowel(String a){
+        if((a.charAt(0)=='a' ||a.charAt(0)=='e' ||a.charAt(0)=='i' ||a.charAt(0)=='o' ||a.charAt(0)=='u') && ((a.charAt(a.length()-1)=='a' ||a.charAt(a.length()-1)=='e' ||a.charAt(a.length()-1)=='i' ||a.charAt(a.length()-1)=='o' ||a.charAt(a.length()-1)=='u'))){
+            return 1;
+        }
+        return 0;
+    }
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int[] ans=new int[words.length];
+        for(int i=0;i<words.length;i++){
+            if(isVowel(words[i])==1){
+                ans[i]=1;
+            }else{
+                ans[i]=0;
+            }
+        }
+        int[] sum=new int[ans.length+1];
+        sum[0]=0;
+        for(int i=1;i<=ans.length;i++){
+            sum[i]=sum[i-1]+ans[i-1];
+        }
+        int[] ans1=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+                int start=queries[i][0];
+                int end=queries[i][1];
+                ans1[i]=sum[end+1]-sum[start];
+        }
+        return ans1;
+    }
 }
