69715d668293464c85a956cb5e78f93889f2ce4b
Rithesh05 <161914815+Rithesh05@users.noreply.github.com>
2025-01-23 09:02:30 +0530

    Time: 26 ms (92.54%), Space: 51.5 MB (71.41%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums); // Step 1: Sort the array
-        
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate values for the first number
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
             
-            int left = i + 1; // Pointer to the next element
-            int right = nums.length - 1; // Pointer to the last element
-            
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    
-                    // Move pointers and avoid duplicates
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++; // Move left pointer to increase the sum
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
                 } else {
-                    right--; // Move right pointer to decrease the sum
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
                 }
             }
         }
-        
-        return ans;
+        return res;        
     }
 }
