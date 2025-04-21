693e2de02e3f59ba8cfcc138429f98d349bb74c2
Rithesh05 <ritheshvellampalli05@gmail.com>
2025-04-21 21:40:27 +0530

    fact

---
+++ RESURSIONA2ZBASICS/factorial.java
@@ -0,0 +1,14 @@
+package RESURSIONA2ZBASICS;
+
+public class factorial {
+    public static int fact(int n){
+        if(n==1){
+            return n;
+        }
+        return n*fact(n-1);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(fact(3));
+    }
+}
