9a5b5dfe102427c27312ff6c737824aabed15f37
Rithesh Vellampalli <161914815+Rithesh05@users.noreply.github.com>
2025-06-12 21:37:33 +0530

    Time: 0 ms (100%), Space: 45.7 MB (35.07%) - LeetHub

---
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int binarysearch(int[] arr){
+        int l = 0;
+        int h = arr.length - 1;
+        int firstNegative = arr.length; // default: no negatives
+
+        while (l <= h) {
+            int mid = (l + h) / 2;
+            if (arr[mid] < 0) {
+                firstNegative = mid; // found a negative, but look left
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return arr.length - firstNegative;
+    }
+
+    public int countNegatives(int[][] grid) {
+        int sum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][grid[i].length - 1] >= 0) {
+                // No negatives in this row
+                continue;
+            }
+            sum += binarysearch(grid[i]);
+        }
+        return sum;
+    }
+}
