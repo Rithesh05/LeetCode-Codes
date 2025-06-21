f2d52758aa3c32583689a3f42c74f19838bb1cb6
Rithesh Vellampalli <161914815+Rithesh05@users.noreply.github.com>
2025-06-21 09:56:01 +0530

    Time: 0 ms (100%), Space: 42.1 MB (67.34%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,22 @@
 class Solution {
+    public double power(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if (n < 0) {
+            return 1.0 / power(x, -n);
+        } else {
+            return power(x, n);
+        }
     }
 }
