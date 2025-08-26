ff9f7c0a45b2bfc797c378d87bbab422e65c6f1e
Rithesh Vellampalli <161914815+Rithesh05@users.noreply.github.com>
2025-08-26 22:43:21 +0530

    Time: 745 ms (11.24%), Space: 0B (100%) - LeetHub

--- 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -1,17 +1,3 @@
--- SELECT * 
--- FROM (
---     SELECT e.employee_id, e.name, s.salary
---     FROM employees e
---     LEFT JOIN salaries s ON e.employee_id = s.employee_id
-    
---     UNION
-    
---     SELECT e.employee_id, e.name, s.salary
---     FROM employees e
---     RIGHT JOIN salaries s ON e.employee_id = s.employee_id
--- ) AS temp;
-
-
 select employee_id from (select employee_id from employees where employee_id not in (
     SELECT employee_id FROM salaries
 )
