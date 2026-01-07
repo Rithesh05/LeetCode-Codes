d02893dafead5ad379b4cb4b5f65318083c03f01
Rithesh Vellampalli <161914815+Rithesh05@users.noreply.github.com>
2026-01-07 15:52:52 +0530

    Time: 4 ms (100%), Space: 56.5 MB (100%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -1,31 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    long MOD = 1000000007L;
-    long ans = 0;
-
-    private long dfs(TreeNode node) {
-        if (node == null) return 0;
-
-        node.val += dfs(node.left) + dfs(node.right);
-        return node.val;
-    }
-
-    public int maxProduct(TreeNode root) {
-        long total = dfs(root);
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-
-        while (!q.isEmpty()) {
-            TreeNode node = q.poll();
-            if (node == null) continue;
-
-            long cur = (total - node.val) * node.val;
-            ans = Math.max(ans, cur);
-
-            if (node.left != null) q.add(node.left);
-            if (node.right != null) q.add(node.right);
+    static long sum;
+    static long max;
+    static int MODULO = 1000000007;
+    // static long MODULO = 1000000007;
+    private static long getSum(TreeNode root){
+        if(root == null){
+            return 0;
         }
+        return root.val + getSum(root.left) + getSum(root.right);
+    }
+    private static long getMaxProduct(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        long left = getMaxProduct(root.left);
+        long right = getMaxProduct(root.right);
 
-        return (int)(ans % MOD);
+        long t1 = left + right + root.val;
+        long temp = (sum-t1) * t1;
+        if(temp > max){
+            max = temp;
+        }
+        return t1;
+    }
+    public static int maxProduct(TreeNode root) {
+        max = 0;
+        sum = getSum(root);
+        getMaxProduct(root);
+        return (int)(max%MODULO);
     }
 }
